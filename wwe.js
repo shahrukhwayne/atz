2724c5a32e3c81e2010f80a55436078d8d0bdfa4
shahrukhwayne <142448240+shahrukhwayne@users.noreply.github.com>
2024-07-14 06:55:44 +0530

    html file

--- wwe.js
+++ wwe.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 8000
+app.use(express.static(path.join(__dirname, 'public')));
 app.get('/',(req,res)=>
 {
-    res.send('Hello srk heroku');
+  
+        res.sendFile(path.join(__dirname, 'public', 'index.html'));
 })
 app.listen(PORT,(err,res)=>
 {
