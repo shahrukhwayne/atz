0489c58156ed9a73adf6b6b0fd5c081b9bf44f7b
shahrukhwayne <142448240+shahrukhwayne@users.noreply.github.com>
2024-07-14 06:47:04 +0530

    change file

--- wwe.js
+++ wwe.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 8000
 app.get('/',(req,res)=>
 {
-    res.send('Hello srk');
+    res.send('Hello srk heroku');
 })
 app.listen(PORT,(err,res)=>
 {
